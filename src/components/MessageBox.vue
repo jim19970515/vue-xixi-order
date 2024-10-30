a832da092d6fc2901176c3db3727599781257987
jim <f0920515972@gmail.com>
2024-10-30 23:38:05 +0800

    提交

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -7,6 +7,7 @@
         <div class="flex items-center justify-center bg-white px-2 rounded-10 ring-4 ring-[#01B93B] z-50">
             <img src="../components/icons/tickIcon.png" class="h-10" alt="">
             <p class="text-[#01B93B] font-extrabold">訂單已送出</p>
+            <h1>123</h1>
         </div>
     </div>
 </template>
