9d2a90e94510a4245c7e542ba30e89ff0bbb1709
jim <f0920515972@gmail.com>
2024-11-15 18:35:07 +0800

    調整為懶加載

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Order from '../views/order.vue'
-import TableSelection from '../views/TableSelection.vue'
-import SelectTable from '@/views/SelectTable.vue'
-import camera from '../components/camera.vue'
-import { useOrderStore } from '@/stores/store'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "TableSelection",
+      component: () => import("@/views/TableSelection.vue"),
+      redirect: "/SelectTable",
+      children: [
         {
-            path: '/',
-            name: 'TableSelection',
-            component: TableSelection,
-            redirect: '/SelectTable',
-            children:[
-                {
-                    path:'SelectTable',
-                    name:'SelectTable',
-                    component: SelectTable
-                },
-                {
-                    path: 'Camera',
-                    name: 'Camera',
-                    component: camera
-                },
-            ]
+          path: "SelectTable",
+          name: "SelectTable",
+          component: () => import("@/views/SelectTable.vue"),
         },
         {
-            path: '/Order/:id',
-            name: 'order',
-            component: Order,
-            async beforeEnter(to,from) {
-                const orderStore = useOrderStore()
-                await orderStore.getProduct()
-            }
+          path: "Camera",
+          name: "Camera",
+          component: () => import("@/components/Camera.vue"),
         },
-        {
-            path:'/CheckOut',
-            name:'CheckOut',
-            component:()=> import('../views/CheckOut.vue')
-        },
-        {
-            path:'/Details/:id',
-            name:'Details',
-            component:()=>import('../views/Details.vue'),
-        },
-    ],
-})
+      ],
+    },
+    {
+      path: "/Order/:id",
+      name: "order",
+      component: () => import("@/views/Order.vue"),
+    },
+    {
+      path: "/CheckOut",
+      name: "CheckOut",
+      component: () => import("@/views/CheckOut.vue"),
+    },
+    {
+      path: "/Details/:id",
+      name: "Details",
+      component: () => import("@/views/Details.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
